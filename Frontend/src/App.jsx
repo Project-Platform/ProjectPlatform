147482b492d7542fc621982f7c98eaca7851be22
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2023-12-19 15:07:59 +0530

    added login/register page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/loginPage';
+import HomePage from './pages/homePage';
+import StudentRegistrationForm from './pages/signUp/student';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/student/register" element={<StudentRegistrationForm />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
