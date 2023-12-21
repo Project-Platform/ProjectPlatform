d20589f0183ac678120f05736a8f7de6837f9f24
msabhinavchandra <msabhinavchandra@gmail.com>
2023-12-21 14:59:35 +0530

    Static HomePage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,66 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/loginPage';
+// import LoginPage from './pages/loginPage';
 import HomePage from './pages/homePage';
-import StudentRegistrationForm from './pages/signUp/student';
+// import StudentRegistrationForm from './pages/signUp/student';
+
+// import Navbar from "./Navbar";
+// import Card from "./Card";
+// import Card2 from "./Card2";
+// import Footer from "./Footer";
+// import Pagination from "./Pagination"
+
+// function HomePage(){
+//   return (<div>
+//     <Navbar/>
+//     <br/>
+//     <div className="ml-28 flex flex-row flex-wrap">
+
+//     <Card2 
+//     name="E-Portal Case management"
+//     descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//     />
+//         <Card2 
+//     name="E-Portal Case management"
+//     descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//     />
+//             <Card2 
+//     name="E-Portal Case management"
+//     descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//     />
+//                   <Card2 
+//       name="E-Portal Case management"
+//       descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//       />
+//                      <Card2 
+//       name="E-Portal Case management"
+//       descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//       />
+//                           <Card2 
+//       name="E-Portal Case management"
+//       descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//       />
+//                           <Card2 
+//       name="E-Portal Case management"
+//       descp="An e-portal case management project is a digital platform designed to streamline and enhance the process of managing cases or incidents within an organization or a specific....."
+//       />
+
+
+
+//     {/* <Card2
+//     name="Breast Cancer detection"
+//     descp=""
+//     /> */}
+//     </div>
+//     <div className="ml-[80vw]"><Pagination/>
+//     </div>
+// <Footer/>
+//     </div>
+//   )
+// };
+
+// export default HomePage;
+
+
 
 function App() {
   return (
@@ -9,8 +68,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/student/register" element={<StudentRegistrationForm />} />
+          {/* <Route path="/login" element={<LoginPage />} />
+          <Route path="/student/register" element={<StudentRegistrationForm />} /> */}
         </Routes>
       </Router>
     </div>
