eae1d96f9e06ccce6c88a84c1cb168f5ec549e4c
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2023-12-25 13:34:54 +0530

    added useEffect to fetch trending projects when visiting homepage.

--- Backend/src/Models/project.js
+++ Backend/src/Models/project.js
@@ -5,82 +5,86 @@ const projectSchema = new Schema({
   // Title of the project, a required string field
   title: {
     type: String,
-    required: true
+    required: true,
   },
 
   // Field of study for the project, a required string field
   field: {
     type: String,
     required: true,
-    default: "Computer Science"
+    default: "Computer Science",
   },
 
   // Domain(s) of the project, an array of strings, required
   domain: {
     type: [String],
-    required: true
+    required: true,
   },
 
   // Mentor's ID, a reference to the 'mentors' collection
   mentorId: {
     type: String,
-    ref: "mentors",   // Reference to the 'mentors' collection
+    ref: "mentors", // Reference to the 'mentors' collection
     required: true,
-    default: "tempMentor"
+    default: "tempMentor",
+    select: false,
   },
 
   // University's ID, a reference to the 'univs' collection
   universityId: {
     type: String,
-    ref: "univs",     // Reference to the 'univs' collection
+    ref: "univs", // Reference to the 'univs' collection
     required: true,
-    default: "tempUnivesity"
+    default: "tempUnivesity",
+    select: false,
   },
 
   // Author(s) of the project, an array of strings, a reference to the 'students' collection
   author: {
     type: [String],
-    ref:"students",      // Reference to the 'students' collection
+    ref: "students", // Reference to the 'students' collection
     required: true
   },
 
   // Abstract of the project, a required string field
   abstract: {
     type: String,
-    required: true
+    required: true,
   },
 
   githubLink: {
-    type: String
-},
+    type: String,
+  },
 
   youtubeLink: {
-    type: String
+    type: String,
   },
 
   // Document's ID, a reference to the 'docs' collection
   docs: {
     type: Schema.Types.ObjectId,
     ref: "docs",
+    select: false
   },
 
   // Date of the project, a string field with a default value
   date: {
     type: String,
     required: false,
-    default: new Date().getFullYear()  // Default value set to the current year
+    default: new Date().getFullYear(), // Default value set to the current year
   },
 
   // Embeddings of the project, an array of numbers, required
   embeddings: {
     type: [Number],
-    required: true
+    required: true,
+    select: false,
   },
   views: {
     type: Number,
     required: true,
-    default: 0
-  }
+    default: 0,
+  },
 });
 
 // Export the project schema for use in creating a model
--- Frontend/src/pages/homePage.jsx
+++ Frontend/src/pages/homePage.jsx
@@ -1,7 +1,28 @@
 import Card2 from "../components/Card2";
 import Pagination from "../components/Pagination";
+import { getTrendingProjects } from "../services/projectData.js";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const [trendingProjects, setTrendingProjects] = useState([]);
+
+  useEffect(() => {
+    // Define a function to fetch trending projects
+    const TrendingProjects = async () => {
+      try {
+        const projects = await getTrendingProjects();
+        setTrendingProjects(projects); // Assuming the response is an array of projects
+      } catch (error) {
+        console.error("Error fetching trending projects:", error);
+      }
+    };
+
+    // Call the function to fetch trending projects
+    TrendingProjects();
+  }, []); // The empty dependency array ensures that this effect runs only once on component mount
+
+  console.log(trendingProjects);
+
   return (
     <div>
       <br />
