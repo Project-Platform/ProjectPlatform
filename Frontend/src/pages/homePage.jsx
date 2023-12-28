333dff0b67d16fc9e3525ccb89757061ae2002c4
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2023-12-28 23:38:38 +0530

    changed the component used for pagination to Material Tailwind

--- Frontend/src/pages/homePage.jsx
+++ Frontend/src/pages/homePage.jsx
@@ -1,14 +1,12 @@
-import Card2 from "../components/Card2";
 import Pagination from "../components/Pagination";
 import { getTrendingProjects } from "../services/projectData";
 import { useState, useEffect } from "react";
-import Pagination2 from "../components/Pagination2";
 import ProjectList from "../components/ProjectList";
 
 function HomePage() {
   const [trendingProjects, setTrendingProjects] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(9);
+  const postsPerPage = 6;
 
   useEffect(() => {
     // Define a function to fetch trending projects
@@ -26,6 +24,7 @@ function HomePage() {
   }, []); // The empty dependency array ensures that this effect runs only once on component mount
 
   console.log(trendingProjects);
+
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentPosts = trendingProjects.slice(firstPostIndex, lastPostIndex);
@@ -34,12 +33,8 @@ function HomePage() {
     <div>
       <br />
         <ProjectList trendingProjects={currentPosts} />
-        <Pagination2
-          totalPosts={trendingProjects.length}
-          postsPerPage={postsPerPage}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        />
+        <Pagination setCurrentPage={setCurrentPage}
+          currentPage={currentPage}/>
     </div>
   );
 }
