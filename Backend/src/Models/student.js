13f0311b9bc6e9143e38091800db670bb5743f5b
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2024-01-01 19:44:22 +0530

    updated mongoose document schemas for student and project.

--- Backend/src/Models/student.js
+++ Backend/src/Models/student.js
@@ -2,62 +2,59 @@ import { Schema, model } from 'mongoose';
 
 
 // Define a Mongoose schema for the 'students' collection
-const studentSchema = new mongoose.Schema({
-    // Full name of the student, a required string field
-    name: {
-        type: String,
-        required: true
-    },
+const studentSchema = new Schema({
+  // Full name of the student, a required string field
+  name: {
+    type: String,
+    required: true,
+  },
 
-    // Unique username for the student, a required string field
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
+  // Unique username for the student, a required string field
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
 
-    // Email of the student, a required string field
-    email: {
-        type: String,
-        required: true
-    },
+  // Email of the student, a required string field
+  email: {
+    type: String,
+    required: true,
+  },
 
-    // Password of the mentor, a required string field
-    password: {
-        type: String,
-        required: true
-    },
+  // Mentor's ID, a reference to the 'mentor' collection
+  mentorId: {
+    type: String,
+    ref: "mentors",
+    required: true,
+    default: "tempMentor",
+    select: false,
+  },
 
-    // Roll number of the student, a required string field
-    rollNo: {
-        type: String,
-        required: true
-    },
+  // University's Name, a reference to the 'univ' collection
+  universityName: {
+    type: String,
+    required: true,
+  },
 
-    // Mentor's ID, a reference to the 'mentor' collection
-    mentorId: {
-        type: String,
-        ref: "mentors",
-        required: true,
-        default: "tempMentor"
+  // Number of projects associated with the student, a required number field with a default of 0
+  projectNo: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  contacts: {
+    githubUsername: {
+      type: String,
+      required: false,
     },
-
-    // University's ID, a reference to the 'univ' collection
-    universityId: {
-        type: String,
-        ref: "univs",
-        required: true,
-        default: "tempUnivesity"
+    linkedinProfile: {
+      type: String,
+      required: false,
     },
-
-    // Number of projects associated with the student, a required number field with a default of 0
-    projectNo: {
-        type: Number,
-        required: true,
-        default: 0
-    },
+  },
 });
 
 
 // Export the user schema for use in creating a model
-export default model('User', userSchema);
+export default model('Student', studentSchema);
