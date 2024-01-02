78fc19d46234284aae4372cb63b28231ba1fe4a4
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2024-01-02 15:06:06 +0530

    added a search bar to navbar.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,12 +8,16 @@ import {
   Typography,
   Button,
   IconButton,
+  Input,
 } from "@material-tailwind/react";
 
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 
 import { useSession, signIn } from "next-auth/react";
-
 
 export function StickyNavbar() {
   const [openNav, setOpenNav] = React.useState(false);
@@ -27,21 +31,6 @@ export function StickyNavbar() {
     );
   }, []);
 
-  const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Search
-        </a>
-      </Typography>
-    </ul>
-  );
-
   return (
     <div className="-m-6 max-h-[768px] w-[calc(100%+24px)]">
       <Navbar className="mt-6 mr-0 sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
@@ -53,8 +42,28 @@ export function StickyNavbar() {
           >
             Project Platform
           </Typography>
+          <div className="hidden items-center gap-x-2 lg:flex">
+            <div className="relative flex w-full gap-2 md:w-max">
+              <Input
+                type="search"
+                placeholder="Search"
+                containerProps={{
+                  className: "min-w-[288px]",
+                }}
+                className=" !border-t-blue-gray-300 pl-9 placeholder:text-blue-gray-300 focus:!border-blue-gray-300"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+              <div className="!absolute left-3 top-[13px]">
+                <MagnifyingGlassIcon className="h-4 w-4" strokeWidth={2} />
+              </div>
+            </div>
+            <Button size="md" className="rounded-lg ">
+              Search
+            </Button>
+          </div>
           <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
             <div className="flex items-center gap-x-1">
               {status === "authenticated" ? (
                 <ProfileMenu image={session.user.image} />
@@ -84,7 +93,34 @@ export function StickyNavbar() {
           </div>
         </div>
         <Collapse open={openNav} className="ml-2">
-          {navList}
+          <br />
+          <div className="container mx-auto">
+            <div className="flex flex-col gap-x-2 sm:flex-row sm:items-center">
+              <div className="relative w-full gap-2 md:w-max">
+                <Input
+                  type="search"
+                  placeholder="Search"
+                  containerProps={{
+                    className: "min-w-[288px]",
+                  }}
+                  className=" !border-t-blue-gray-300 pl-9 placeholder:text-blue-gray-300 focus:!border-blue-gray-300"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <div className="!absolute left-3 top-[13px]">
+                  <MagnifyingGlassIcon
+                    className="h-4 w-4"
+                    fill="black"
+                    strokeWidth={2}
+                  />
+                </div>
+              </div>
+              <Button size="md" className="mt-1 rounded-lg sm:mt-0">
+                Search
+              </Button>
+            </div>
+          </div>
         </Collapse>
       </Navbar>
     </div>
