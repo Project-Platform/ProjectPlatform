8824decf78a3e463468298bcc1d7b432895e9864
GuruPrasadTS <pguruprasad204@gmail.com>
2024-01-02 14:14:45 +0530

    Done with responsiveness of Project Upload

--- Frontend/src/components/TextArea.jsx
+++ Frontend/src/components/TextArea.jsx
@@ -1,9 +1,11 @@
 import { Textarea } from "@material-tailwind/react";
 
-function TextArea(props){
-    return(<div className="flex p-3 md:p-3 col-span-2 md:col-span-1 lg:col-span-2 w-full md:w-auto">
-    <Textarea label={props.label}/>    
-</div>);
+function TextArea(props) {
+  return (
+    <div className="md:col-span-2 lg:col-span-1 p-3 md:p-3 w-full">
+      <Textarea label={props.label} />
+    </div>
+  );
 }
 
 export default TextArea;
