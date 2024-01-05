d6d4f58f765db92b2448dadd6739de2918a39f08
GuruPrasadTS <pguruprasad204@gmail.com>
2024-01-05 16:28:30 +0530

    added comma for domain in project view

--- Frontend/src/components/ProjectView.jsx
+++ Frontend/src/components/ProjectView.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 export default function ProjectView(props) {
   const myArray = props.author;
   const arrayElementsWithSpaces = myArray.join(', ');
+  const domArray = props.domain;
+  const domainElementswithSpace = domArray.join(', ');
   return (
     <div className="min-h-screen">
       <h1 className="font-semibold text-5xl ml-[18vw] mt-[3vw] underline underline-offset-auto ">
@@ -15,7 +17,7 @@ export default function ProjectView(props) {
       <h2 className="font-medium text-lg ml-[18vw]">Date: {props.date}</h2>
 
       <h2 className="font-medium text-lg ml-[18vw]">
-        Domain: {props.domain1}, {props.domain2}
+        Domain: {domainElementswithSpace}
       </h2>
 
       <p className="p-5 mx-60  text-xl ">{props.abstract}</p>
