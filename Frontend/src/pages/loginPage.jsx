8dd65e75bae092bc21a7ab5116256d0ffaa4a99c
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2024-01-17 01:04:48 +0530

    added functionality to both github and google oauth

--- Frontend/src/pages/loginPage.jsx
+++ Frontend/src/pages/loginPage.jsx
@@ -3,31 +3,26 @@ import {
   CardHeader,
   CardBody,
   Typography,
-  Button,
 } from "@material-tailwind/react";
-
-import { signIn } from "next-auth/react";
 
 export function LoginPage() {
   return (
-    <form className="flex justify-center items-center h-screen ">
+    <div className="flex justify-center items-center h-[80vh]">
       <Card className="w-96">
         <CardHeader
           variant="gradient"
           color="gray"
-          className="mb-4 grid h-28 place-items-center"
+          className="mb-4 grid h-28 place-content-center"
         >
           <Typography variant="h3" color="white">
             Log In
           </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4">
-          <Button
-            size="lg"
-            variant="outlined"
-            color="blue-gray"
-            fullWidth
-            className="flex place-content-center gap-3 mt-3"
+          <Typography
+            as="a"
+            href="/api/auth/google"
+            className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg border border-blue-gray-500 text-blue-gray-500 hover:opacity-75 focus:ring focus:ring-blue-gray-200 active:opacity-[0.85] w-full flex place-content-center gap-3 mt-3"
           >
             <img
               src="../../icons8-google.svg"
@@ -35,14 +30,11 @@ export function LoginPage() {
               className="h-6 w-6"
             />
             Log in with Google
-          </Button>
-          <Button
-            size="lg"
-            variant="outlined"
-            color="blue-gray"
-            fullWidth
-            className="flex place-content-center gap-3 mt-3"
-            onClick={() => signIn()}
+          </Typography>
+          <Typography
+            as="a"
+            href="/api/auth/github"
+            className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg border border-blue-gray-500 text-blue-gray-500 hover:opacity-75 focus:ring focus:ring-blue-gray-200 active:opacity-[0.85] w-full flex place-content-center gap-3 mt-3"
           >
             <img
               src="../../github-mark.svg"
@@ -50,10 +42,10 @@ export function LoginPage() {
               className="h-6 w-6"
             />
             Log in with GitHub
-          </Button>
+          </Typography>
         </CardBody>
       </Card>
-    </form>
+    </div>
   );
 }
 export default LoginPage;
