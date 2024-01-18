0265ddaf5b47442d10c8780f179e02188f337fee
princi <princi@kali>
2024-01-18 20:27:22 +0530

    Reapply "front end smol changes" (#52)
    This reverts commit 47c55adae18971b2a6d1bf532503e1e9ddaafea8, reversing
    changes made to c0aed68b0a8a7f909b046ea36ce76d858a986e00.

--- Frontend/src/components/ProjectList.jsx
+++ Frontend/src/components/ProjectList.jsx
@@ -1,12 +1,20 @@
 import ProjectCard from "./ProjectCard";
+import React from "react";
+import {
+  Typography,
+} from "@material-tailwind/react";
 
 const ProjectList = ({ trendingProjects }) => {
   return (
     <div>
-      <h1 className="text-2xl  font-semibold text-gray-800 m-4 mt-12 flex flex-row place-content-center">
-        Trending Projects
-      </h1>
-      <div className="flex flex-row flex-wrap place-content-center">
+      <div className="flex flex-wrap flex-col justify-center items-center bg-black">
+        <Typography variant="h1" color="white" className="m-10 mt-28 text-center">PROJECT PLATFORM</Typography>
+        <div className="ml-5 mr-5 mb-20 mt-12 flex justify-center items-center">
+          <Typography variant="h5" color="white" className="text-center">A unified space for students to showcase their projects, exchange knowledge, and foster innovation, creating a hub for collaborative learning and knowledge sharing.</Typography>
+        </div>
+      </div>
+      <Typography variant="h4" color="black" className="flex justify-center mt-6 underline">Trending Projects</Typography>
+      <div className="mt-2 flex flex-row flex-wrap place-content-evenly">
         {trendingProjects.map((curr) => {
           return (
             <ProjectCard
