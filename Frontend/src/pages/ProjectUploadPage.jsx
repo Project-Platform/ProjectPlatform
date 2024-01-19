521a88470de1f05b445fe52ae5973b6213ad2c25
SreeVibha12 <vibhasree2811@gmail.com>
2024-01-19 15:49:21 +0530

    projectuploadpage form component

--- Frontend/src/pages/ProjectUploadPage.jsx
+++ Frontend/src/pages/ProjectUploadPage.jsx
@@ -1,180 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { TagsInput } from "react-tag-input-component";
-import { Button, Input, Textarea } from "@material-tailwind/react";
-import { addProject } from "../services/projectData";
+import { Button } from "@material-tailwind/react";
 import AlertBox from "../components/AlertBox";
-
+import ProjectUploadForm from "../components/ProjectUploadForm";
+import { addProject } from "../services/projectData";
 
 function ProjectUploadPage() {
-  const [projectData, setProjectData] = useState({
-    title: "",
-    author: [],
-    domain: [],
-    abstract: "",
-    docs: null,
-  });
+  const [projectData, setProjectData] = useState({ title: "", author: [], domain: [], abstract: "", docs: null });
   const [message, setMessage] = useState(null);
 
   useEffect(() => {
-   
     const storedData = JSON.parse(localStorage.getItem("projectData"));
-    if (storedData) {
-      setProjectData(storedData);
-    }
+    storedData && setProjectData(storedData);
   }, []);
 
   const handleInputChange = (field, value) => {
-    setProjectData((prevData) => {
-      const newData = { ...prevData, [field]: value };
-
-      // Store the updated data in local storage
-      localStorage.setItem("projectData", JSON.stringify(newData));
-
-      return newData;
-    });
+    const newData = { ...projectData, [field]: value };
+    setProjectData(newData);
+    localStorage.setItem("projectData", JSON.stringify(newData));
   };
 
-
-const handleFileChange = (e) => {
-  const file = e.target.files[0];
-
-  if (file) {
-    setProjectData({ ...projectData, docs: file });
-
-    // Save the updated projectData including the document in local storage
-    localStorage.setItem("projectData", JSON.stringify({ ...projectData, docs: file }));
-  }
-};
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    file && setProjectData({ ...projectData, docs: file });
+    file && localStorage.setItem("projectData", JSON.stringify({ ...projectData, docs: file }));
+  };
 
   const handleUploadProject = async () => {
     try {
-      console.log(projectData);
       localStorage.setItem("projectData", JSON.stringify(projectData));
       const newProject = await addProject(projectData);
-      console.log("Project uploaded successfully:", newProject);
-
-      setMessage({type: "success", message: "Project successfully uploaded."});
-
+      setMessage({ type: "success", message: "Project successfully uploaded." });
       localStorage.removeItem("projectData");
-      
-      // setProjectData({
-      //   title: "",
-      //   author: [],
-      //   domain: [],
-      //   abstract: "",
-      //   docs: null,
-      // });
     } catch (error) {
       setMessage({ type: "error", message: "Project failed to upload." });
       console.error("Error uploading project:", error);
     }
   };
 
-  const isFormValid = () => {
-    const requiredFields = ["title", "author", "domain", "abstract", "docs"];
-    
-    for (const field of requiredFields) {
-      if (!projectData[field]) {
-        return false; 
-      }
-    }
-  
-    return true; 
-  };
-
+  const isFormValid = () => ["title", "author", "domain", "abstract", "docs"].every((field) => projectData[field]);
 
   return (
     <>
-      {message && (
-        <AlertBox
-          type={message.type}
-          message={message.message}
-          onClose={setMessage}
-        />
-      )}
+      {message && <AlertBox type={message.type} message={message.message} onClose={setMessage} />}
       <div className="mt-10">
-        <h1 className="flex justify-center mt-3 mb-6 text-3xl md:text-5xl text-bold">
-          Project Upload
-        </h1>
+        <h1 className="flex justify-center mt-3 mb-6 text-3xl md:text-5xl text-bold">Project Upload</h1>
       </div>
       <div className="flex justify-center m-0 p-0">
         <section className="flex flex-col md:flex-row mx-auto p-2 md:p-4 shadow-md rounded-8 max-w-4xl">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (isFormValid()) {
-                handleUploadProject();
-              } else {
-                setMessage({ type: "error", message: "Please fill in all required fields." });
-              }
-            }}
-            
-            encType="multipart/form-data"
-            className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 text-left"
-          >
-            <div className="md:py-3 flex place-items-center pl-4 col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl">
-              Idea Title (100 characters):
-            </div>
-            <Input
-              label="Title"
-              onChange={(e) => {
-                handleInputChange("title", e.target.value);
-              }}
-              value={projectData.title}
-              containerProps={{ className: "place-self-center" }}
-            />
-
-            <div className="md:py-3 flex place-items-center pl-4 col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl">
-              Author(s) Username:
-            </div>
-            <TagsInput
-              value={projectData.author}
-              onChange={(value) => handleInputChange("author", value)}
-              name="Author"
-              placeHolder="Enter Author Name:"
-            />
-
-            <div className="md:py-3 flex place-items-center pl-4 col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl">
-              Domain (AIML, Blockchain, etc):
-            </div>
-            <TagsInput
-              value={projectData.domain}
-              onChange={(value) => handleInputChange("domain", value)}
-              name="Domain"
-              placeHolder="Enter Domain:"
-            />
-
-            <div className="md:py-14 flex place-items-center pl-4 col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl content-center">
-              Abstract (1500 characters):
-            </div>
-            <Textarea
-              label="Abstract"
-              onChange={(e) => handleInputChange("abstract", e.target.value)}
-              value={projectData.abstract}
-            />
-
-            <div className="md:py-3 flex place-items-center pl-4 col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl">
-              Document (PDF or word format, up to 500Kb):
-            </div>
-            <div className="flex place-items-center col-span-2 md:col-span-1 rounded-4 text-18 font-bold text-xl">
-              <input
-                type="file"
-                accept=".pdf, .doc, .docx"
-                onChange={handleFileChange}
-                multiple
-              />
-            </div>
-
-            <div className="flex flex-col justify-center items-center mx-auto col-span-2">
-              <Button className="mb-4" variant="filled" size="lg" type="submit">
-                Plagiarism Checker
-              </Button>
-              <br />
-              <Button variant="filled" size="lg" type="submit">
-                Upload Project
-              </Button>
-            </div>
-          </form>
+          <ProjectUploadForm
+            projectData={projectData}
+            setProjectData={setProjectData}
+            handleFileChange={handleFileChange}
+            handleUploadProject={handleUploadProject}
+            isFormValid={isFormValid}
+            setMessage={setMessage}
+          />
         </section>
       </div>
     </>
